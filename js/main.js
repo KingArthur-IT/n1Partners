d7836d5bb6f8c03c6fa5d8d84ae4123bee1b906a
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-12-05 18:40:14 +0300

    edits

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ document.querySelectorAll('.letter').forEach((el) => {
     el.addEventListener('click', () => {
         if (modal){
             modal.classList.add('displayed');
+            document.querySelector('body').classList.add('overflow-hidden');
             setTimeout(() => {
                 modal.classList.add('visible');
             }, 300);
@@ -16,18 +17,14 @@ document.querySelectorAll('.letter').forEach((el) => {
 const closeModal = () => {
     if (modal){
         modal.classList.remove('visible');
+        document.querySelector('body').classList.remove('overflow-hidden');
         setTimeout(() => {
             modal.classList.remove('displayed');
         }, 300);
     }
 };
 
-document.querySelector('.wins-modal__close')?.addEventListener('click', () => {
-    closeModal();
-});
-
-modal.addEventListener('click', () => {
-    closeModal();
-});
+document.querySelector('.wins-modal__close')?.addEventListener('click', () => closeModal());
+modal.addEventListener('click', () => closeModal());
 
 document.querySelector('.wins-modal__hero').addEventListener('click', e => e.stopPropagation())
